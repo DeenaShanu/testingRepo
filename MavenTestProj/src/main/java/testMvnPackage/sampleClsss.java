91da66e299544a582db0f4501bbf8727447b59f3
shanu <shanu@BOOK-4QTRLAP57K>
2024-12-04 10:19:40 +0530

    committed t

---
+++ MavenTestProj/src/main/java/testMvnPackage/sampleClsss.java
@@ -0,0 +1,11 @@
+package testMvnPackage;
+
+public class sampleClsss {
+
+	public static void main(String[] args) {
+		String tes = "Hello" ;
+		System.out.println(tes.length());
+		String a = new String("Google");
+		System.out.println(a.length());
+	}
+}
